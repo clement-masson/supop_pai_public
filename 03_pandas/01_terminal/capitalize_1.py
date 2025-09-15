6728cac51498111668ee6ee076fdeb9c116932de
Clément <masson.cle@gmail.com>
2025-09-16 00:21:37 +0200

    suppression fichiers copiés par erreur

--- 03_pandas/01_terminal/capitalize_1.py
+++
@@ -1,8 +0,0 @@
-input_path = "remplacer par le chemin de votre fichier"
-output_path = "remplacer par le chemin de votre fichier"
-
-with open(input_path, "r") as f:
-    content = f.read().upper()
-
-with open(output_path, "w") as f:
-    f.write(content)
